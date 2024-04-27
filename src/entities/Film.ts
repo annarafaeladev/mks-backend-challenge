703a2bb660d3a14c1eebecf9870a3291a93736b9
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2024-04-27 01:23:17 -0300

    feat: setup entities database

---
+++ src/entities/Film.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { IsNotEmpty, MaxLength } from 'class-validator';
+import { User } from './User';
+
+@Entity("films")
+export class Film {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({type: "text", nullable: false})
+  @IsNotEmpty({ message: 'O título é obrigatório' })
+  @MaxLength(100, { message: 'O título deve ter no máximo 100 caracteres' })
+  title: string;
+
+  @Column({type: "text", nullable: false})
+  @IsNotEmpty({ message: 'A descrição é obrigatória' })
+  description: string;
+
+  @ManyToOne(() => User, user => user.films)
+  @JoinColumn({name: "user_id"})
+  user: User;
+
+  constructor(title: string, description: string) {
+    this.title = title;
+    this.description = description;
+  }
+}
---
+++ src/entities/User.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { IsNotEmpty, IsEmail, MinLength, MaxLength } from 'class-validator';
+import { Film } from './Film';
+
+@Entity("users")
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @MinLength(3, { message: 'O nome deve ter no mínimo 3 caracteres' })
+  @MaxLength(50, { message: 'O nome deve ter no máximo 50 caracteres' })
+  name: string;
+
+  @Column({type: "text", nullable: false})
+  @IsEmail({}, { message: 'Email inválido' })
+  email: string;
+
+  @Column({type: "text", nullable: false})
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
+  password: string;
+
+  @OneToMany(() => Film, film => film.user)
+  films: Film[];
+
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email   
+    this.password = password;
+  }
+
+
+}
